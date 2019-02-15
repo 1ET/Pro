7880d55d0edb9009c5823efa68b1b7a8ab5fc30b
LH <1374052652@qq.com>
2019-02-15 10:43:31 +0800

    配置css样式

--- src/components/login.vue
+++ src/components/login.vue
@@ -1,15 +1,16 @@
 <template>
-
-  <el-form :label-position="labelPosition" label-width="80px" :model="formdata">
-    <h2>用户登录</h2>
-    <el-form-item label="用户名">
-      <el-input v-model="formdata.username"></el-input>
-    </el-form-item>
-    <el-form-item label="密码">
-      <el-input v-model="formdata.password"></el-input>
-    </el-form-item>
-    <el-button type="success" class="login-btn">登录</el-button>
-  </el-form>
+  <div class="login-warp">
+    <el-form :label-position="labelPosition" label-width="80px" :model="formdata" class="login-form">
+      <h2>用户登录</h2>
+      <el-form-item label="用户名">
+        <el-input v-model="formdata.username"></el-input>
+      </el-form-item>
+      <el-form-item label="密码">
+        <el-input v-model="formdata.password"></el-input>
+      </el-form-item>
+      <el-button type="success" class="login-btn">登录</el-button>
+    </el-form>
+  </div>
 </template>
 
 <script>
@@ -27,4 +28,20 @@ export default {
 </script>
 
 <style>
+.login-warp {
+  background: #324152;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.login-form {
+  background-color: #ffffff;
+  border-radius: 5px;
+  width: 400px;
+  padding: 30px;
+}
+.login-btn {
+  width: 100%;
+}
 </style>
